a20ab22a68cec577fa1d1790a5bb038f5aac4a6f
The-Old-Cat <artkov476@gmail.com>
2025-11-25 10:18:33 +0300

    Настроены счетчики : Количество инфобаз Количество сессий Количество рабочих процессов rphost.exe

--- scripts/1c_sessions.ps1
+++ scripts/1c_sessions.ps1
@@ -4,13 +4,13 @@ try {
     $RacPath = "C:\Program Files\1cv8\8.3.27.1786\bin\rac.exe"
     
     if ($Metric -eq "count") {
-        $result = & $RacPath process list --cluster=c7313ba1-5875-4498-8184-4a830f12d77f --cluster-user=new_1cPin --cluster-pwd=!Admin1c!159753 localhost:1545 2>$null
+        $result = & $RacPath session list --cluster=c7313ba1-5875-4498-8184-4a830f12d77f --cluster-user=new_1cPin --cluster-pwd=!Admin1c!159753 localhost:1545 2>$null
         
         # Безопасная проверка на null
         if ($LASTEXITCODE -eq 0 -and $result -ne $null) {
             $count = 0
             foreach ($line in $result) {
-                if ($line -ne $null -and $line -match "^\s*process\s*:") {
+                if ($line -ne $null -and $line -match "^\s*session\s*:") {
                     $count++
                 }
             }
